1a9b7709df7119b1e28081366149e984d410ac89
Quant-King <totalityemperor@gmail.com>
2026-08-04 23:24:27 +0100

    Add main function to display user information

--- to-do.cpp
+++ to-do.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
+int main() {
+  string name = "Malik";
+  int age = 17;
+  string goal = "Become a master at C++ and python";
+  cout << "My name is" << name << "\n";
+  cout   << "I am " << age << "\n";
+  cout << "My goal is to " << goal;
+  return 0;
+}
